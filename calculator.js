c67768c6b4312a3a7e1a8aab08d7b72e5e7fc3bf
JeanFabry <jeanfabry1435@gmail.com>
2020-11-16 16:49:14 +0100

    Complete calculator

--- calculator.js
+++ calculator.js
@@ -1,43 +1,91 @@
 const body = document.querySelector("body");
+body.style.display = "flex";
+body.style.flexDirection="column";
+body.style.alignItems = "center";
+body.style.width="50%";
 const ecran = document.createElement("div");
+ecran.style.border = "1px solid black";
+ecran.style.height = "75px";
+ecran.style.width= "50%";
 
-for (let i = 0; i <= 9; i++) {
-  const button = document.createElement("button");
-  button.textContent = i;
+const keyboard = document.createElement("div");
+keyboard.style.display="flex";
+keyboard.style.flexDirection="column";
+keyboard.style.border = "1px solid black";
+keyboard.style.width= "50%";
 
-  body.appendChild(button);
+    const floorA = document.createElement("div");
+    floorA.style.display="flex";
+    floorA.style.justifyContent = "space-between";
 
-  button.addEventListener("click", function () {
-    ecran.textContent += String(i);
-  });
-}
+    const floorB = document.createElement("div");
+    floorB.style.display = "flex";
+    floorB.style.justifyContent = "space-between";
 
-const operations= ["+","-","*","/"]
-for (let i=0; i<= operations.length-1;i++) {
-const op = document.createElement("button");
-op.textContent = operations[i];
-body.appendChild(op);
-op.addEventListener("click", function () {
-  ecran.textContent += operations[i];
-});
-}
+    const floorC = document.createElement("div");
+    floorC.style.display = "flex";
+    floorC.style.justifyContent = "space-between";
 
-const c = document.createElement("button");
-c.textContent = "C";
-body.appendChild(c);
-c.addEventListener("click", function () {
-  ecran.textContent = "";
-});
-
-
-const equal = document.createElement("button");
-equal.textContent = "=";
-body.appendChild(equal);
-equal.addEventListener("click", function (){
-  ecran.textContent=Function('return ' + ecran.textContent)();
-});
-
+    const floorD = document.createElement("div");
+    floorD.style.display="flex";
+    floorD.style.justifyContent = "space-between";
+    
+    const floorE = document.createElement("div");
+    floorE.style.display = "flex";
+    floorE.style.justifyContent = "space-between";
 
 body.appendChild(ecran);
-ecran.style.border = "1px solid black";
-ecran.style.height = "100px";
+body.appendChild(keyboard);
+keyboard.appendChild(floorA);
+keyboard.appendChild(floorB);
+keyboard.appendChild(floorC);
+keyboard.appendChild(floorD);
+keyboard.appendChild(floorE);
+
+
+const operations = ["(", ")", "%", "AC", "7", "8", "9", "/", "4", "5", "6", "x", "1", "2", "3", "-", "0", ".", "=", "+"]
+for (let i=0; i<= operations.length-1;i++) {
+    const op = document.createElement("button");
+    op.textContent = operations[i];
+    op.style.width="25%";
+    
+    // body.appendChild(op);
+
+    let cases = operations[i];
+    switch(cases){
+
+        case "AC":
+        op.addEventListener("click", function () {
+        ecran.textContent = "";})
+        break;
+
+        case "=":
+        op.addEventListener("click", function (){
+        ecran.textContent=Function('return ' + ecran.textContent)();
+        });
+        break;
+
+        default:
+        op.addEventListener("click", function () {
+        ecran.textContent += operations[i];
+        });   
+    };   
+
+     if (i <= 3) {
+       floorA.appendChild(op);
+     }
+     if ((i > 3) && (i <= 7)) {
+       floorB.appendChild(op);
+     }
+     if ((i > 7) && (i <= 11)) {
+       floorC.appendChild(op);
+     }
+     if (i > 11 && i <= 15) {
+       floorD.appendChild(op);
+     }
+     if (i > 15 && i <= 19) {
+       floorE.appendChild(op);
+     }
+}
+
+
