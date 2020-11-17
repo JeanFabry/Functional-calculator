4c6cf2b6dc399b49531ec693a8bc0622526b021e
JeanFabry <jeanfabry1435@gmail.com>
2020-11-17 10:26:11 +0100

    Previous Number done

--- calculator.js
+++ calculator.js
@@ -1,91 +1,87 @@
+//-------------- Hierarchy -------------
+const html = document.querySelector("html");
 const body = document.querySelector("body");
-body.style.display = "flex";
-body.style.flexDirection="column";
-body.style.alignItems = "center";
-body.style.width="50%";
 const ecran = document.createElement("div");
-ecran.style.border = "1px solid black";
-ecran.style.height = "75px";
-ecran.style.width= "50%";
+    const calcul = document.createElement("div");
+    ecran.appendChild(calcul);
+
+    const response = document.createElement("div");
+    ecran.appendChild(response);
 
 const keyboard = document.createElement("div");
-keyboard.style.display="flex";
-keyboard.style.flexDirection="column";
-keyboard.style.border = "1px solid black";
-keyboard.style.width= "50%";
-
-    const floorA = document.createElement("div");
-    floorA.style.display="flex";
-    floorA.style.justifyContent = "space-between";
-
-    const floorB = document.createElement("div");
-    floorB.style.display = "flex";
-    floorB.style.justifyContent = "space-between";
-
-    const floorC = document.createElement("div");
-    floorC.style.display = "flex";
-    floorC.style.justifyContent = "space-between";
-
-    const floorD = document.createElement("div");
-    floorD.style.display="flex";
-    floorD.style.justifyContent = "space-between";
+const subKeyboard = document.createElement("div");
+const floorA = document.createElement("div");
+const floorB = document.createElement("div");
+const floorC = document.createElement("div");
+const floorD = document.createElement("div");
+const floorE = document.createElement("div");
     
-    const floorE = document.createElement("div");
-    floorE.style.display = "flex";
-    floorE.style.justifyContent = "space-between";
-
+keyboard.appendChild(subKeyboard);
+subKeyboard.appendChild(floorA);
+subKeyboard.appendChild(floorB);
+subKeyboard.appendChild(floorC);
+subKeyboard.appendChild(floorD);
+subKeyboard.appendChild(floorE);
+    
+html.appendChild(body);
 body.appendChild(ecran);
 body.appendChild(keyboard);
-keyboard.appendChild(floorA);
-keyboard.appendChild(floorB);
-keyboard.appendChild(floorC);
-keyboard.appendChild(floorD);
-keyboard.appendChild(floorE);
+//------------End Hierarchy -------------
 
-
-const operations = ["(", ")", "%", "AC", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"]
-for (let i=0; i<= operations.length-1;i++) {
-    const op = document.createElement("button");
-    op.textContent = operations[i];
-    op.style.width="25%";
+//------------Buttons and operations -------------
+const operations = ["(", ")", "%","AC","7","8","9","/", "4","5", "6","*","1", "2","3","-","0",".","=","+",];
+for (let i = 0; i <= operations.length - 1; i++) {
+  //loop creation of buttons for each values of the array
+  const op = document.createElement("button");
+  op.textContent = operations[i];
+  //------------A bit of style for the buttons -------------
+  op.style.width = "17%";
+  op.style.height = "65px";
+  op.style.borderRadius = "20%";
+  op.style.backgroundColor = "rgb(168, 163, 175)";
+  op.style.color = "white";
+  op.style.fontSize = "20px";
+  op.style.fontWeight = "bold";
+  op.style.fontFamily = "Arial";
+  op.style.boxShadow = "2px 2px 5px rgb(63, 60, 68) ";
+  //------------End of bit of style -------------
     
-    // body.appendChild(op);
+  //------------- Operations --------------------
+  let cases = operations[i];
+  switch (cases) {
+    case "AC":
+      op.addEventListener("click", function () {
+        response.textContent = "";
+      });
+      break;
 
-    let cases = operations[i];
-    switch(cases){
+    case "=":
+      op.addEventListener("click", function () {
+        response.textContent = Function("return " + ecran.textContent)();
+      });
+      op.style.backgroundColor = "rgb(58, 147, 255)";
+      break;
 
-        case "AC":
-        op.addEventListener("click", function () {
-        ecran.textContent = "";})
-        break;
+    default:
+      op.addEventListener("click", function () {
+        calcul.textContent += operations[i];
+      });
+  }
+  //------------- End Operations --------------------
 
-        case "=":
-        op.addEventListener("click", function (){
-        ecran.textContent=Function('return ' + ecran.textContent)();
-        });
-        break;
-
-        default:
-        op.addEventListener("click", function () {
-        ecran.textContent += operations[i];
-        });   
-    };   
-
-     if (i <= 3) {
-       floorA.appendChild(op);
-     }
-     if ((i > 3) && (i <= 7)) {
-       floorB.appendChild(op);
-     }
-     if ((i > 7) && (i <= 11)) {
-       floorC.appendChild(op);
-     }
-     if (i > 11 && i <= 15) {
-       floorD.appendChild(op);
-     }
-     if (i > 15 && i <= 19) {
-       floorE.appendChild(op);
-     }
+  if (i <= 3) {
+    floorA.appendChild(op);
+  }
+  if (i > 3 && i <= 7) {
+    floorB.appendChild(op);
+  }
+  if (i > 7 && i <= 11) {
+    floorC.appendChild(op);
+  }
+  if (i > 11 && i <= 15) {
+    floorD.appendChild(op);
+  }
+  if (i > 15 && i <= 19) {
+    floorE.appendChild(op);
+  }
 }
-
-
---
+++ style.js
@@ -0,0 +1,65 @@
+html.style.display = "flex";
+html.style.flexDirection = "column";
+html.style.alignItems = "center";
+html.style.backgroundImage = "url('./Assets/background.jpg')";
+html.style.backgroundSize="cover";
+
+body.style.display = "flex";
+body.style.flexDirection = "column";
+body.style.alignItems = "center";
+body.style.width = "100%";
+body.style.marginTop="5%";
+
+ecran.style.boxSizing="border-box";
+ecran.style.borderRadius="30% 30% 0% 0%";
+ecran.style.border = "30px solid rgb(94, 86, 107)";
+// ecran.style.lineHeight = "125px";
+ecran.style.height = "125px";
+ecran.style.paddingTop = "12px";
+ecran.style.width = "400px";
+ecran.style.boxShadow = "15px 20px 20px rgb(60, 53, 71)";
+ecran.style.backgroundColor = "rgb(242, 239, 247)";
+ecran.style.textAlign="right";
+ecran.style.verticalAlign = "center";
+
+calcul.style.fontFamily = "Arial";
+calcul.style.fontSize = "16px";
+calcul.style.fontWeight = "bold";
+calcul.style.marginRight="5px";
+
+response.style.fontFamily = "Arial";
+response.style.fontSize = "22px";
+response.style.marginTop = "4px";
+response.style.fontWeight = "bold";
+response.style.marginRight="5px";
+
+
+keyboard.style.display = "flex";
+keyboard.style.justifyContent="center";
+keyboard.style.height = "420px";
+keyboard.style.width = "400px";
+keyboard.style.borderRadius = "0% 0% 15% 15%";
+keyboard.style.backgroundColor="rgb(94, 86, 107)";
+keyboard.style.boxShadow = "15px 20px 20px rgb(60, 53, 71)";
+
+subKeyboard.style.width ="350px";
+subKeyboard.style.marginTop ="10px";
+
+floorA.style.display = "flex";
+floorA.style.justifyContent = "space-around";
+floorA.style.margin="10px 0";
+
+floorB.style.display = "flex";
+floorB.style.justifyContent = "space-around";
+
+floorC.style.display = "flex";
+floorC.style.justifyContent = "space-around";
+floorC.style.margin = "10px 0";
+
+floorD.style.display = "flex";
+floorD.style.justifyContent = "space-around";
+
+floorE.style.display = "flex";
+floorE.style.justifyContent = "space-around";
+floorE.style.margin = "10px 0";
+
